fcd9cebd228fbd09fcca4865ee067c2f51f4c3cf
Gyuri Horak <dyuri@horak.hu>
2014-10-01 15:50:42 +0200

    multiple probes, configuration from server side

--- ws.py
+++ ws.py
@@ -8,22 +8,86 @@ from ws4py.async_websocket import WebSocket
 from ws4py.server.tulipserver import WebSocketProtocol
 
 websockets = []
+probes = []
+
+
+class Probe():
+
+    def __init__(self, name, valueCount=1):
+        self.name = name
+        self.valueCount = valueCount
+
+    def get_values(self):
+        raise NotImplementedError()
+
+    def get_configuration(self):
+        raise NotImplementedError()
+
+
+class CpuProbe(Probe):
+
+    def __init__(self):
+        super().__init__("cpu", psutil.cpu_count() or 1)
+
+    def get_configuration(self):
+        return {
+            "name": self.name,
+            "valueCount": self.valueCount,
+            "extent": [0, 100],
+        }
+
+    # TODO cache, history (timestamp)
+    def get_values(self):
+        cpu_percent = psutil.cpu_percent(None, True)
+        ts = int(datetime.datetime.now().timestamp() * 1000)
+
+        return {
+            "values": cpu_percent,
+            "timestamp": ts,
+        }
+
+
+class SingleCpuProbe(Probe):
+
+    def __init__(self):
+        super().__init__("cpu-single")
+
+    def get_configuration(self):
+        return {
+            "name": self.name,
+            "valueCount": self.valueCount,
+            "extent": [0, 100],
+            "height": 4,
+            "colors": ["#006d2c", "#31a354", "#74c476", "#bae4b3", "#bdd7e7", "#6baed6", "#3182bd", "#08519c"],
+        }
+
+    def get_values(self):
+        cpu_percent = psutil.cpu_percent()
+        ts = int(datetime.datetime.now().timestamp() * 1000)
+
+        return {
+            "values": cpu_percent,
+            "timestamp": ts,
+        }
+
+
+probes.append(CpuProbe())
+probes.append(SingleCpuProbe())
+
 
 @asyncio.coroutine
 def sysinfo():
     while True:
-        cpu_percent = psutil.cpu_percent(None, True)
-        ts = int(datetime.datetime.now().timestamp() * 1000)
-        response = {
-            "cpu": {
-                "values": cpu_percent,
-                "timestamp": ts
-            }
-        }
-        json_response = simplejson.dumps(response)
+        response = { probe.name: probe.get_values() for probe in probes }
+
+        json_response = simplejson.dumps({ "data": response })
         for ws in websockets:
             ws.send(json_response)
         yield from asyncio.sleep(1)
+
+
+def configuration():
+    return { probe.name: probe.get_configuration() for probe in probes }
 
 
 def start_server(loop):
@@ -44,6 +108,7 @@ class SysInfoWebSocket(WebSocket):
 
     def opened(self):
         websockets.append(self)
+        self.send(simplejson.dumps({ "configuration": configuration() }))
 
     def closed(self, code, reason=None):
         websockets.remove(self)
